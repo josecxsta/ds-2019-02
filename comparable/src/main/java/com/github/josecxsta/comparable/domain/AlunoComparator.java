5cacfae15879bf5d43b61e5451c7188c3ab57d9e
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2019-10-01 08:49:42 -0300

    Sugestão

--- comparable/src/main/java/com/github/josecxsta/comparable/domain/AlunoComparator.java
+++ comparable/src/main/java/com/github/josecxsta/comparable/domain/AlunoComparator.java
@@ -2,6 +2,8 @@ package com.github.josecxsta.comparable.domain;
 
 import java.util.Comparator;
 
+// FIXME O nome pode ser melhor, pois representa uma operação relevante para o domínio (um bom nome aqui é questão de design). 
+// Ter uma classe especificamente para dizer quem vem antes de quem, para um dado cenário, é design. 
 public class AlunoComparator implements Comparator<Aluno> {
 
     public int compare(Aluno a, Aluno b)
